141c6a42ad4e4ad3b9139f45115b179571e9dbce
syk25 <syk2245@gmail.com>
2024-06-18 20:00:36 +0900

    Register Page 구현

---
+++ src/components/Register.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Register = () => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({ username, email, password });
+        setEmail("");
+        setUsername("");
+        setPassword("");
+    };
+    return (
+        <main className='register'>
+            <h1 className='registerTitle'>Create an account</h1>
+            <form className='registerForm' onSubmit={handleSubmit}>
+                <label htmlFor='username'>Username</label>
+                <input
+                    type='text'
+                    name='username'
+                    id='username'
+                    required
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <label htmlFor='email'>Email Address</label>
+                <input
+                    type='text'
+                    name='email'
+                    id='email'
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <label htmlFor='password'>Password</label>
+                <input
+                    type='password'
+                    name='password'
+                    id='password'
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button className='registerBtn'>REGISTER</button>
+                <p>
+                    Have an account? <Link to='/'>Sign in</Link>
+                </p>
+            </form>
+        </main>
+    );
+};
+
+export default Register;
