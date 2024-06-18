b13aacb0d0dd73125aae0d50611d85a8dae95650
syk25 <syk2245@gmail.com>
2024-06-18 21:20:05 +0900

    회원가입시 서버에게 데이터 전송

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,55 +2,87 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log({ username, email, password });
-        setEmail("");
-        setUsername("");
-        setPassword("");
-    };
-    return (
-        <main className='register'>
-            <h1 className='registerTitle'>Create an account</h1>
-            <form className='registerForm' onSubmit={handleSubmit}>
-                <label htmlFor='username'>Username</label>
-                <input
-                    type='text'
-                    name='username'
-                    id='username'
-                    required
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <label htmlFor='email'>Email Address</label>
-                <input
-                    type='text'
-                    name='email'
-                    id='email'
-                    required
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <label htmlFor='password'>Password</label>
-                <input
-                    type='password'
-                    name='password'
-                    id='password'
-                    required
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button className='registerBtn'>REGISTER</button>
-                <p>
-                    Have an account? <Link to='/'>Sign in</Link>
-                </p>
-            </form>
-        </main>
-    );
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		signUp(); // 회원가입 정보 제출 시 서버에게 정보 전송
+		setEmail("");
+		setUsername("");
+		setPassword("");
+	};
+	return (
+		<main className="register">
+			<h1 className="registerTitle">Create an account</h1>
+			<form
+				className="registerForm"
+				onSubmit={handleSubmit}
+			>
+				<label htmlFor="username">Username</label>
+				<input
+					type="text"
+					name="username"
+					id="username"
+					required
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<label htmlFor="email">Email Address</label>
+				<input
+					type="text"
+					name="email"
+					id="email"
+					required
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<label htmlFor="password">Password</label>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					required
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button className="registerBtn">REGISTER</button>
+				<p>
+					Have an account? <Link to="/">Sign in</Link>
+				</p>
+			</form>
+		</main>
+	);
 };
 
 export default Register;
+
+/* 서버에게 회원가입 정보 전송 */
+const navigate = useNavigate();
+
+const signUp = () => {
+    fetch("http://localhost:4000/api/register", {
+        method: "POST",
+        body: JSON.stringify({
+            email,
+            password,
+            username,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.error_message) {
+                alert(data.error_message);
+            } else {
+                alert("Account created successfully!");
+                navigate("/");
+            }
+        })
+        .catch((err) => console.error(err));
+};
+
+
