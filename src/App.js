55575a3e001d9f7c24ab2e92ca4ba17c23420dbe
syk25 <syk2245@gmail.com>
2024-06-18 19:46:06 +0900

    App.js 수정(Login, Register, Home, Replies 매핑)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 
-function App() {
-  return (
-      <div>
-          <p>Hello World!</p>
-      </div>
-  );
-}
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Replies from "./components/Replies";
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/dashboard' element={<Home />} />
+                    <Route path='/:id/replies' element={<Replies />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
+
 export default App;
