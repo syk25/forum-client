dd45d482e9f4d444f83d2f80848029ff9e71754b
syk25 <syk2245@gmail.com>
2024-06-18 22:44:03 +0900

    로그인 후 포스트 정보 요청 및 표시

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Nav from "./Nav";
 
 const Home = () => {
-    const [thread, setThread] = useState("");
+	const [thread, setThread] = useState("");
+	const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log({ thread });
-        setThread("");
-    };
-    return (
-        <>
-            <Nav />
-            <main className='home'>
-                <h2 className='homeTitle'>Create a Thread</h2>
-                <form className='homeForm' onSubmit={handleSubmit}>
-                    <div className='home__container'>
-                        <label htmlFor='thread'>Title / Description</label>
-                        <input
-                            type='text'
-                            name='thread'
-                            required
-                            value={thread}
-                            onChange={(e) => setThread(e.target.value)}
-                        />
-                    </div>
-                    <button className='homeBtn'>CREATE THREAD</button>
-                </form>
-            </main>
-        </>
-    );
+	// 사용자가 인가되었는지 확인
+	useEffect(() => {
+		const checkUser = () => {
+			if (!localStorage.getItem("_id")) {
+				navigate("/");
+			} else {
+				console.log("Authenticated");
+			}
+		};
+		checkUser();
+	}, [navigate]);
+
+	const createThread = () => {
+		fetch("http://localhost:4000/api/create/thread", {
+			method: "POST",
+			body: JSON.stringify({
+				thread,
+				userId: localStorage.getItem("_id"),
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+			})
+			.catch((err) => console.error(err));
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		createThread(); // 스레드 생성 함수 호출
+		setThread("");
+	};
+
+	return (
+		<>
+			<Nav />
+			<main className="home">
+				<h2 className="homeTitle">Create a Thread</h2>
+				<form
+					className="homeForm"
+					onSubmit={handleSubmit}
+				>
+					<div className="home__container">
+						<label htmlFor="thread">Title / Description</label>
+						<input
+							type="text"
+							name="thread"
+							required
+							value={thread}
+							onChange={(e) => setThread(e.target.value)}
+						/>
+					</div>
+					<button className="homeBtn">CREATE THREAD</button>
+				</form>
+			</main>
+		</>
+	);
 };
 
 export default Home;
